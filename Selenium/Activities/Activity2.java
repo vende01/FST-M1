25fbd80bf706982d1667cb03992966a36bdaf746
vende01 <venugopaldigital@gmail.com>
2026-02-03 20:55:50 +0530

    Add files via upload

--- Selenium/Activities/Activity2.java
+++ Selenium/Activities/Activity2.java
@@ -1,35 +1,31 @@
-package selenium;
+package example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Activity2 {
-    public static void main(String[] args) {
-        // Initialize the Firefox driver
-        WebDriver driver = new FirefoxDriver();
 
-        // Open the page
-        driver.get("https://training-support.net/webelements/login-form");
-        // Print the title of the page
-        System.out.println("Page title: " + driver.getTitle());
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://training-support.net/webelements/login-form/");
+		System.out.println(driver.getTitle());
+		
+		WebElement username = driver.findElement(By.xpath("//*[@id='username']"));
+		username.sendKeys("admin");
+		
+		WebElement password = driver.findElement(By.xpath("//*[@id='password']"));
+		password.sendKeys("password");
+		
+		WebElement submit = driver.findElement(By.xpath("//button[contains(text(),'Submit')]"));
+		submit.click();
+		
+		System.out.println(driver.getTitle());
+		driver.quit();
 
-        // Find the username field and enter the username
-        driver.findElement(By.id("username")).sendKeys("admin");
-        // Find the password field and enter the password
-        driver.findElement(By.id("password")).sendKeys("password");
-        // Find the login button and click it
-        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+	}
 
-        // Print the confirmation message
-        String message = driver.findElement(By.tagName("h1")).getText();
-        System.out.println(message);
-
-        // Close the browser
-        driver.quit();
-    }
 }
--- Selenium/Activities/Activity5.java
+++ Selenium/Activities/Activity5.java
@@ -1,8 +1,4 @@
-package selenium;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+package example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
